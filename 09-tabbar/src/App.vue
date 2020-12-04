9b3dd2356d8292ec1e0778f98a2d6244f2aebdd3
jinxiazz <2929547617@qq.com>
2020-12-04 18:14:39 +0800

    11

---
+++ 09-tabbar/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <TabBar>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.png">
+        <div>首页</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.png">
+        <div>分类</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.png">
+        <div>购物车</div>
+      </div>
+      <div class="tab-bar-item">
+        <img src="../../assets/img/tabbar/home.png">
+        <div>我的</div>
+      </div>
+    </TabBar>
+  </div>
+</template>
+
+<script>
+  //引入tab-bar
+  import TabBar from "./components/tabbar/TabBar";
+  export default {
+    name: 'App',
+    components:{
+      TabBar
+    }
+  }
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+
+</style>
